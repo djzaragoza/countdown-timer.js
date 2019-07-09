08d1b72ac3469dba1662e8aa11ca36f1b7a10bed
DJ Zaragoza <zaragoza0126@gmail.com>
2019-07-09 06:34:04 -0700

    built html layout of timer

--- countdown.js
+++ countdown.js
@@ -1,37 +1,40 @@
 function countdown(endDate) {
-   let days, hours, minutes, seconds;
-   
-   endDate = new Date(endDate).getTime();
+    let days, hours, minutes, seconds;
 
-   if(isNaN(endDate)) {
-       return;
-   }
-   setInterval(calculate, 1000);
+    endDate = new Date(endDate).getTime();
 
-   function calculate() {
-       let startDate = new Date();
-       startDate = startDate.getTime();
+    if (isNaN(endDate)) {
+        return;
+    }
 
-       let timeRemaining = parseInt((endDate - startDate) / 1000);
+    setInterval(calculate, 1000);
 
-       if (timeRemaining >= 0) {
-           days = parseInt(timeRemaining / 86400);
-           timeRemaining = (timeRemaining % 86400);
+    function calculate() {
+        let startDate = new Date();
+        startDate = startDate.getTime();
 
-           hours = parseInt(timeRemaining / 3600);
-           timeRemaining = (timeRemaining % 3600);
+        let timeRemaining = parseInt((endDate - startDate) / 1000);
 
-           minutes = parseInt(timeRemaining / 60);
-           timeRemaining = (timeRemaining % 60);
+        if (timeRemaining >= 0) {
+            days = parseInt(timeRemaining / 86400);
+            timeRemaining = (timeRemaining % 86400);
 
-           document.getElementById("days").innerHTML = parseInt(days, 10);
-           document.getElementById("hours").innerHTML = ("0" + hours).slice(-2);
-           document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
-           document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
-       } else {
-           return;
-       }
-   }
+            hours = parseInt(timeRemaining / 3600);
+            timeRemaining = (timeRemaining % 3600);
+
+            minutes = parseInt(timeRemaining / 60);
+            timeRemaining = (timeRemaining % 60);
+
+            seconds = parseInt(timeRemaining);
+
+            document.getElementById("days").innerHTML = parseInt(days, 10);
+            document.getElementById("hours").innerHTML = ("0" + hours).slice(-2);
+            document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
+            document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
+        } else {
+            return;
+        }
+    }
 }
 
 (function () {
